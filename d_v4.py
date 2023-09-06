56893ee6bb913d12d52d383a7f5d1aed16b07ce0
xalil8 <halil.ozcan1999@gmail.com>
2023-09-06 16:37:23 +0300

    perspective transform added

---
+++ d_v4.py
@@ -0,0 +1,210 @@
+import torch
+import cv2
+import math
+import numpy as np
+import time
+from collections import defaultdict, deque
+from trackers.multi_tracker_zoo import create_tracker
+import itertools
+
+# Load the detection model
+model = torch.hub.load("ultralytics/yolov5", "yolov5m", device="mps")
+model.conf = 0.5
+class_names = model.names
+model.classes = [0]
+tracker_list = create_tracker(f'ocsort', f"trackers/ocsort/configs/ocsort.yaml", "weights/osnet_x0_25_msmt17.pt", device=torch.device("mps"), half=False)
+
+# H = np.array([[-5.11754792e+00, -4.55824434e+00  ,6.68141996e+03],
+#                 [-1.84946589e-01, -7.70514232e+00 , 3.14970818e+03],
+#                 [-1.21118016e-04, -7.97458811e-03,  1.00000000e+00]])
+
+H = np.array([[-1.03589760e+01 ,-1.03238523e+01 , 1.39468683e+04],
+                [-2.24633122e-01 ,-1.63955809e+01,  6.39756256e+03],
+                [ 2.95906560e-06, -8.95928977e-03 , 1.00000000e+00]])
+
+def point_transform(x_old,y_old):
+    H = np.array([[-1.03589760e+01 ,-1.03238523e+01 , 1.39468683e+04],
+                    [-2.24633122e-01 ,-1.63955809e+01,  6.39756256e+03],
+                    [ 2.95906560e-06, -8.95928977e-03 , 1.00000000e+00]])
+    # Create the homogeneous coordinate for the source point
+    source_point = np.array([x_old, y_old, 1])
+    # Perform the transformation
+    transformed_point = np.dot(H, source_point)
+
+    # Normalize the transformed point
+    x_new = int(transformed_point[0] / transformed_point[2])
+    y_new = int(transformed_point[1] / transformed_point[2])
+    return int(x_new),int(y_new) 
+
+# Define a function to draw center points on the bird's-eye view image
+def draw_birdseye_view(center_points):
+
+    birdseye_view = np.zeros((720, 1280, 3), dtype=np.uint8)
+    for center in center_points:
+        # Draw the center point on the bird's-eye view image
+        cv2.circle(birdseye_view, center, 5, (0, 0, 255), -1)
+    
+    return birdseye_view
+
+def clean_dict(main_dict,count):
+    
+    keys_to_delete = []  # Create a list to store keys to delete
+
+    for pair_key, pair_data in main_dict.items():
+        if (count - sliding_windows[pair_key]["last_count"] > disappear_limit):
+            keys_to_delete.append(pair_key)
+            print(f"{pair_key} CLEANED")
+    # Delete items outside the loop
+    for key in keys_to_delete:
+        del main_dict[key]
+    return main_dict
+
+
+    
+# Open the video capture
+source_video_path = "sari_yelek_v8.mp4"
+video_cap = cv2.VideoCapture(source_video_path)
+
+# Define the distance threshold for considering persons as close
+distance_threshold = 300  # in pixels
+
+# Define time intervals for checking proximity
+check_interval = 15 * 60  # 15 minutes in seconds
+
+# Define the sliding window size
+window_size = 300
+disappear_limit = 100
+# Dictionary to store sliding windows for each person pair
+sliding_windows = {}
+counter = 0
+while video_cap.isOpened():
+    counter += 1 
+    last_time = time.time()
+
+    ret, frame = video_cap.read()
+    if not ret:
+        break
+    
+    tracking_list = []
+
+    results = model(frame)
+    det = results.xyxy[0]
+
+
+
+    bird_eye_view  = np.zeros((1440, 2560, 3), dtype=np.uint8)
+    cv2.polylines(bird_eye_view, [np.array( [[1046, 424], [649, 1150], [1902, 1115], [1433, 418]], dtype=np.int32)], isClosed=True, color=(0, 0, 255), thickness=2)
+    bird_eye_view2  = np.zeros((1440, 2560, 3), dtype=np.uint8)
+    cv2.polylines(bird_eye_view2, [np.array([[point_transform(961, 383)], [point_transform(164, 1242)], [point_transform(2493, 1217)], [point_transform(1573, 377)]], dtype=np.int32)], isClosed=True, color=(0, 0, 255), thickness=2)
+
+
+
+
+
+    if det is not None and len(det):
+        outputs = tracker_list.update(det.cpu(), frame)
+
+        # Extract the IDs from the outputs
+        ids = outputs[:, 4]
+        
+        for j, (x1, y1, x2, y2, id, cls, conf) in enumerate(outputs[:, :7]):
+            x1, y1, x2, y2, id = map(int, (x1, y1, x2, y2, id))
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 2)
+            cv2.putText(frame, f"{str(int(conf * 100))}  person{id}", (x1 + 10, y1 - 20), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.8, (0, 0, 255), 2)
+
+        # Loop through all unique pairs of IDs
+        unique_ids = np.unique(ids)
+        for id1, id2 in itertools.combinations(unique_ids, 2):
+            id1, id2 = int(id1), int(id2)  # Convert IDs to integers
+            pair_key = f"{id1}-{id2}"
+
+            # Find the corresponding bounding boxes for id1 and id2
+            bbox1 = outputs[ids == id1][0, :4]
+            bbox2 = outputs[ids == id2][0, :4]
+
+            # center1 = ((bbox1[0] + bbox1[2]) / 2, (bbox1[1] + bbox1[3]) / 2)
+            # center2 = ((bbox2[0] + bbox2[2]) / 2, (bbox2[1] + bbox2[3]) / 2)
+            center1 = ((bbox1[0] + bbox1[2]) / 2, bbox1[3])
+            center2 = ((bbox2[0] + bbox2[2]) / 2, bbox2[3])
+            
+            transformed_center1 = point_transform(center1[0],center1[1])
+            transformed_center2 = point_transform(center2[0],center2[1])
+
+            transformed_frame = cv2.warpPerspective(frame, H, (frame.shape[1], frame.shape[0]))
+
+
+
+            # bird_eye_view = cv2.circle(bird_eye_view, (int(center1[0]),int(center1[1])), 5, (0, 0, 255), -1)
+            # bird_eye_view = cv2.circle(bird_eye_view, (int(center2[0]),int(center2[1])), 5, (0, 0, 255), -1)
+            # #cv2.line(bird_eye_view, (int(center1[0]), int(center1[1])), (int(center2[0]), int(center2[1])), (0,255,20), 6)
+
+            # Calculate the Euclidean distance between the centers
+            transformed_distance = int(np.linalg.norm(np.array(transformed_center1) - np.array(transformed_center2)))
+            
+            distance = int(np.linalg.norm(np.array(center1) - np.array(center2)))
+
+
+
+            if pair_key not in sliding_windows:
+                sliding_windows[pair_key] = {"elements":deque(maxlen=window_size),"last_count":None}
+                #sliding_windows[pair_key] = deque(maxlen=window_size)
+
+            sliding_windows[pair_key]["last_count"] = counter
+            # Label the frame based on distance
+            if transformed_distance < distance_threshold:
+                color = (0, 0, 255)
+                sliding_windows[pair_key]["elements"].append(True)
+            else:
+                color = (0, 255, 255)
+                sliding_windows[pair_key]["elements"].append(False)
+
+            # Check if %80 of the 100 elements in the sliding window are True
+            if len(sliding_windows[pair_key]) == window_size and sliding_windows[pair_key].count(True) / window_size >= 0.9:
+                # Add your desired logic here
+                pass
+            
+            cv2.putText(frame, f"{distance}", (int((center1[0] + center2[0]) / 2), int((center1[1] + center2[1]) / 2)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
+            cv2.line(frame, (int(center1[0]), int(center1[1])), (int(center2[0]), int(center2[1])), color, 6)
+            
+            #### transformed
+            cv2.putText(transformed_frame, f"{transformed_distance}", (int((transformed_center1[0] + transformed_center2[0]) / 2), int((transformed_center1[1] + transformed_center2[1]) / 2)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
+            cv2.line(transformed_frame, (int(transformed_center1[0]), int(transformed_center1[1])), (int(transformed_center2[0]), int(transformed_center2[1])), color, 6)
+
+
+
+        #print(sliding_windows["1-3"]["elements"])
+    fps = int(1 / (time.time() - last_time))
+    cv2.putText(frame, f"{fps}", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (209, 20, 209), 4)
+
+
+
+    if (counter %100) == 0:
+        print("CLEANING CHECKED ")
+        sliding_windows = clean_dict(sliding_windows,counter)
+            
+    # for pair_key, pair_data in sliding_windows.items():
+    #     print(counter,sliding_windows[pair_key]["last_count"])
+    # print("//////////////////////////////////////////////////")
+    
+    transformed_frame = cv2.resize(transformed_frame, (800, 520))
+    frame = cv2.resize(frame, (800, 520))
+    
+    
+    # bird_eye_view = cv2.resize(bird_eye_view, (800, 520))
+    # bird_eye_view2 = cv2.resize(bird_eye_view2, (800, 520))
+    stacked = np.hstack((frame,transformed_frame))
+    
+    
+    
+    cv2.imshow("Frame1", stacked)
+    # Press 'q' to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release resources
+video_cap.release()
+cv2.destroyAllWindows()
+
